daf66517c66d19a9ac65537ef1a1a80f0e028b90
Yauheniya <ya.kavalchuk@gmail.com>
2023-09-23 17:03:17 +0300

    add custom picker

--- components/Picker.jsx
+++ components/Picker.jsx
@@ -1,17 +1,79 @@
-import { Text, View } from "react-native";
-import { Picker } from "@react-native-picker/picker";
+import {
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
 import * as React from "react";
-import styles from "../styles/labelsLeftStyles";
+import styles from "../styles/inputStyles";
+import { Ionicons } from "@expo/vector-icons";
 
 const SelectOption = ({ label, options, selectedValue, onValueChange }) => {
+  const [inputValue, setInputValue] = React.useState("");
+  const [open, setOpen] = React.useState(true);
+
+  const onInputChange = (text) => {
+    setInputValue(text);
+  };
+
+  const filteredOptions = options.filter((option) =>
+    option.value.toLowerCase().includes(inputValue.toLowerCase())
+  );
+
+  React.useEffect(() => {
+    const selectedOptionObject = options.find(
+      (option) => option.id === selectedValue
+    );
+    if (selectedValue) {
+      setInputValue(selectedOptionObject.value);
+    }
+  }, [selectedValue]);
+
+  const onItemSelected = (option) => {
+    onValueChange(option.id);
+    setInputValue(option.value);
+    setOpen(false);
+  };
+
+  const renderPickerItem = (option) => {
+    return (
+      <TouchableOpacity
+        key={option.id}
+        onPress={() => {
+          onItemSelected(option);
+        }}
+        style={styles.pickerItem}
+      >
+        <Text style={styles.pickerItemText}>{option.value}</Text>
+      </TouchableOpacity>
+    );
+  };
+
   return (
     <View>
       <Text style={styles.header}>{label}</Text>
-      <Picker selectedValue={selectedValue} onValueChange={onValueChange}>
-        {options.map((option) => (
-          <Picker.Item key={option.id} label={option.value} value={option.id} />
-        ))}
-      </Picker>
+      <View style={styles.inputContainer}>
+        <View>
+          <TextInput
+            style={styles.input}
+            onChangeText={onInputChange}
+            onPressIn={() => setOpen((open) => !open)}
+            value={inputValue}
+          />
+        </View>
+        <Ionicons
+          name={"caret-down"} // Use appropriate icon names
+          size={24}
+          color="#000" // Set your desired icon color
+          style={styles.icon}
+        />
+        {open ? (
+          <ScrollView style={styles.pickerItems}>
+            {filteredOptions.map(renderPickerItem)}
+          </ScrollView>
+        ) : null}
+      </View>
     </View>
   );
 };
---
+++ styles/inputStyles.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from "react-native";
+
+const inputStyles = StyleSheet.create({
+  inputContainer: {
+    position: "relative"
+  },
+  input: {
+    backgroundColor: "#ffffff",
+    borderRadius: 10,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    fontSize: 18,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  icon: {
+    position: "absolute",
+    right: 10,
+    top: "25%",
+  },
+  pickerItems: {
+    maxHeight: 350,
+    position: "absolute",
+    width: "100%",
+    zIndex: 10,
+    backgroundColor: "white",
+    top: "100%",
+    borderRadius: 10,
+  },
+  pickerItem: {
+    paddingVertical: 10,
+    paddingLeft: 5,
+    borderBottomWidth: 2,
+    borderColor: "#eee"
+  },
+  pickerItemText: {
+    fontSize: 18,
+  }
+});
+
+export default inputStyles;
